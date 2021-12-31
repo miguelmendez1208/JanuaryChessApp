99f45020b07368902115d3f534a9d0782fa26c7b
miguelmendez1208 <miguelmendez1208@gmail.com>
2021-12-31 17:50:45 -0600

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var colour = 1;
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Login.html');
+});
+
+io.on('connection', (socket) => {
+  console.log("Assigned colour" + colour);
+  io.emit('serverColor', colour);
+  colour = colour *-1;
+
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
